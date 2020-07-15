abbc394b9e2e81c3b2294c4192a91a6a96ab0213
Hiercy <volk10051@gmail.com>
2020-07-15 15:10:38 +0300

    Encode user password

--- src/main/java/com/mike/blog/service/AuthService.java
+++ src/main/java/com/mike/blog/service/AuthService.java
@@ -4,25 +4,32 @@ import com.mike.blog.dto.RegisterDto;
 import com.mike.blog.model.Customer;
 import com.mike.blog.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
     private final CustomerRepository customerRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public AuthService(CustomerRepository customerRepository) {
+    public AuthService(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
         this.customerRepository = customerRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void register(RegisterDto registerDto) {
         Customer customer = new Customer().builder()
                 .username(registerDto.getUsername())
-                .password(registerDto.getPassword())
+                .password(encodePassword(registerDto.getPassword()))
                 .email(registerDto.getEmail())
                 .build();
 
         customerRepository.save(customer);
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
     }
 }
